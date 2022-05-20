1f0ef477012b82b1403b3f0824766d66831ec296
sarveshsharma <Sarvesh.Sharma@ril.com>
2022-05-20 14:12:36 +0530

    Added merge subarray implementation in go and java

--- GO-Lang/Problems/Arrays/Merge2SortedArrays/Merge2SortedArrays.go
+++ GO-Lang/Problems/Arrays/Merge2SortedArrays/Merge2SortedArrays.go
@@ -3,14 +3,14 @@ package main
 import "fmt"
 
 /**
-*Merge Two Sorted Arrays
+* Merge Two Sorted Arrays
 *
-*Given two sorted arrays A and B, find the merged sorted array C by merging A and B.
+* Given two sorted arrays A and B, find the merged sorted array C by merging A and B.
 *
-*Example:
-*A: [1, 2, 3, 4, 4]
-*B: [2, 4, 5, 5]
-*C: [1, 2, 2, 3, 4, 4, 4, 5, 5]
+* Example:
+* A: [1, 2, 3, 4, 4]
+* B: [2, 4, 5, 5]
+* C: [1, 2, 2, 3, 4, 4, 4, 5, 5]
 **/
 
 func Merge2SortedArrays(A *[]int, B *[]int) []int {
@@ -47,6 +47,54 @@ func Merge2SortedArrays(A *[]int, B *[]int) []int {
 	return C
 }
 
+/**
+ * Consider an array that is divided into two parts and both of the parts are sorted individually.
+ * Given the mentioned array and the end index of the first part,
+ * merge them to create a sorted array. Update the same array with the merged elements.
+ * You can use extra auxiliary space.
+ *
+ * Expected Time Complexity: O(n) where n denotes the size of the array.
+ *
+ * Example
+ * Array: [1, 3, 5, 7, 9, 11, 0, 4, 6, 8]
+ * End Index: 5
+ * Array after merging: [0, 1, 3, 4, 5, 6, 7, 8, 9, 11]
+ */
+
+func Merge(arr *[]int, index int) {
+	C := make([]int, len(*arr))
+
+	i, j, k := 0, index+1, 0
+
+	for i <= index && j < len(*arr) {
+		if (*arr)[i] <= (*arr)[j] {
+			C[k] = (*arr)[i]
+			k++
+			i++
+		} else {
+			C[k] = (*arr)[j]
+			k++
+			j++
+		}
+	}
+
+	for i <= index {
+		C[k] = (*arr)[i]
+		k++
+		i++
+	}
+
+	for j < len(*arr) {
+		C[k] = (*arr)[j]
+		k++
+		j++
+	}
+
+	for i2, v := range C {
+		(*arr)[i2] = v
+	}
+}
+
 func main() {
 	A := []int{1, 2, 3, 4, 4}
 	B := []int{2, 4, 5, 5}
@@ -54,4 +102,9 @@ func main() {
 	C := Merge2SortedArrays(&A, &B)
 
 	fmt.Println("-------->", C)
+
+	arr := []int{1, 3, 5, 7, 9, 11, 0, 4, 6, 8}
+	Merge(&arr, 5)
+	fmt.Println("------->", arr)
+
 }
--- Java/Problems/Arrays/Merge2SortedArrays.java
+++ Java/Problems/Arrays/Merge2SortedArrays.java
@@ -43,11 +43,66 @@ public class Merge2SortedArrays {
         return C;
     }
 
+    /**
+     * Consider an array that is divided into two parts and both of the parts are sorted individually.
+     * Given the mentioned array and the end index of the first part,
+     * merge them to create a sorted array. Update the same array with the merged elements.
+     * You can use extra auxiliary space.
+     *
+     * Expected Time Complexity: O(n) where n denotes the size of the array.
+     *
+     * Example
+     * Array: [1, 3, 5, 7, 9, 11, 0, 4, 6, 8]
+     * End Index: 5
+     * Array after merging: [0, 1, 3, 4, 5, 6, 7, 8, 9, 11]
+     *
+     * @param arr
+     * @param endIndex
+     */
+    void merge (int[] arr, int endIndex) {
+        int i = 0, k = 0;
+        int j = endIndex+1;
+
+        int[] C = new int[arr.length];
+        while (i<=endIndex && j<arr.length){
+            if (arr[i]<=arr[j]){
+                C[k] = arr[i];
+                k++;
+                i++;
+            }else {
+                C[k] = arr[j];
+                k++;
+                j++;
+            }
+        }
+
+        while (i<= endIndex){
+            C[k] = arr[i];
+            k++;
+            i++;
+        }
+
+        while (j< arr.length){
+            C[k] = arr[j];
+            k++;
+            j++;
+        }
+
+        for (int x=0; x< arr.length; x++){
+            arr[x] = C[x];
+        }
+    }
+
     public static void main(String[] args) {
         int[] A = new int[]{1, 2, 3, 4, 4};
         int[] B = new int[]{2, 4, 5, 5};
 
         int [] C = new Merge2SortedArrays().mergeSortedArrays(A,B);
         Arrays.stream(C).forEach(i -> System.out.println("--->"+i));
+
+        int[] arr = new int[]{1, 3, 5, 7, 9, 11, 0, 4, 6, 8};
+
+        new Merge2SortedArrays().merge(arr, 5);
+        Arrays.stream(arr).forEach(i1 -> System.out.println("***---->"+i1));
     }
 }
